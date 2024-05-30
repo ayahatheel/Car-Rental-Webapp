9c11691aa13e80f9b6154fed7db5e1e9adfe719c
Ayah Atheel <66213838+ayahatheel@users.noreply.github.com>
2024-05-30 17:19:43 +0300

    login page ui

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import bgImage from "../Widgets/Images/formbg.png";
+import "../Widgets/login.css";
 
 function Login() {
-    return (
-        <>
-        <h1>login</h1>
-        </>
-    )
+  return (
+    <div
+      className="login-container"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <div className="login-form">
+        <h2>تسجيل الدخول</h2>
+        <p>مرحبا بك مجددا</p>
+        <form>
+          <input type="number" placeholder="رقم الهاتف"/>
+          <input type="password" placeholder="كلمة المرور" />
+          <div className="button-container">
+            <button type="submit">→ تسجيل الدخول</button>
+          </div>
+        </form>
+        <p className="signup-link">
+          ليس لديك حساب؟ <a href="/signup">أنشئ حساباً</a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
