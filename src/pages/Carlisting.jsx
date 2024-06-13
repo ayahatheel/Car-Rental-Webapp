7d0afc6cdff4945ef477dd1f4f2ca9642ea3f615
Ayah Atheel <66213838+ayahatheel@users.noreply.github.com>
2024-06-13 11:54:50 +0300

    carlisting page

--- src/pages/Carlisting.jsx
+++ src/pages/Carlisting.jsx
@@ -3,11 +3,30 @@ import Ads from '../Widgets/Ads'
 import FAQ from '../Widgets/FAQ'
 import CarCard from "../Widgets/CarCard";
 import { Link } from "react-router-dom"; 
+import { Typography } from '@mui/material';
+import Categories from "../Widgets/Categories";
+
 
 function Carlisting() {
     return (
         <>
+          <Typography variant="h6" sx={{ margin: '10px 100px 0 0', padding: 0, textAlign: 'right' }}>
+اخر العروض      </Typography>
         <Ads/>
+
+        <Typography variant="h5" sx={{ margin: '30px 100px 0 0', padding: 0, textAlign: 'right' }}>
+      اختر حسب الفئة      </Typography>
+
+      <Categories />
+
+        <div>
+      <Typography variant="h6" sx={{ margin: '10px 100px 0 0', padding: 0, textAlign: 'right' }}>
+        استكشف اختياراتك
+      </Typography>
+      <Typography variant="h6" sx={{ margin: '0 100px 30px 0px', padding: 0, textAlign: 'right' }}>
+        واختر سيارتك المثالية لمغامرتك القادمة
+      </Typography>
+    </div>
 
         <div className="card-grid" >
         <Link
@@ -38,8 +57,93 @@ function Carlisting() {
         >
           <CarCard />
         </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
       </div>
       <div class="button-container">
+ 
   <button class="load-button">المزيد من السيارات</button>
 </div>
         </>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,15 +7,60 @@ import "../Widgets/CarCard.css";
 import Categories from "../Widgets/Categories";
 import Testimonial from "../Widgets/Testimonial";
 import Chat from "../Widgets/Chat";
+import { Typography } from '@mui/material';
 import AdvertisementModal from "../Widgets/AdvertisementModal";
+
+
 
 function Homepage() {
   return (
     <>
     <AdvertisementModal/>
       <Mainsentn />
+
+      <Typography variant="h5" sx={{ margin: '30px 100px 0 0', padding: 0, textAlign: 'right' }}>
+      اختر حسب الفئة      </Typography>
+
       <Categories />
+
+      <div>
+      <Typography variant="h5" sx={{ margin: '10px 100px 0 0', padding: 0, textAlign: 'right' }}>
+        استكشف اختياراتك
+      </Typography>
+      <Typography variant="h6" sx={{ margin: '0 100px 30px 0px', padding: 0, textAlign: 'right' }}>
+        واختر سيارتك المثالية لمغامرتك القادمة
+      </Typography>
+    </div>
+
       <div className="card-grid">
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
+        <Link
+          to="/Carderails"
+          style={{ textDecoration: "none", color: "inherit" }}
+          className="card-grid-item"
+        >
+          <CarCard />
+        </Link>
         <Link
           to="/Carderails"
           style={{ textDecoration: "none", color: "inherit" }}
@@ -46,7 +91,11 @@ function Homepage() {
         </Link>
       </div>
       <div class="button-container">
-  <button class="load-button">المزيد من السيارات</button>
+
+      <Link to="/Carlisting">
+  <button className="load-button">المزيد من السيارات</button>
+</Link>
+
 </div>
 
       <FAQ />
