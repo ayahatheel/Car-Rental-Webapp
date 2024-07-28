6898594d88fe1227b2c6e7e5ae72f10b47c0a96d
Ayah Atheel <66213838+ayahatheel@users.noreply.github.com>
2024-07-28 10:01:15 +0300

    netlif rent form

--- src/Widgets/CarRentalform.jsx
+++ src/Widgets/CarRentalform.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { useAuth } from '../contexts/authContext'; 
-import { Snackbar, Alert } from '@mui/material';
+import { Alert } from '@mui/material';
 import "./CarRentalform.css";
 
 function CarRentalform() {
