bb07f91e6bc6c2e625c780ab987cc0e2c28b6004
Ayah Atheel <66213838+ayahatheel@users.noreply.github.com>
2024-08-31 13:33:01 +0300

    chat

--- src/Widgets/Chat.jsx
+++ src/Widgets/Chat.jsx
@@ -4,7 +4,7 @@ const Chat = () => {
   useEffect(() => {
     const script = document.createElement('script');
     script.id = 'ze-snippet';
-    script.src = 'https://static.zdassets.com/ekr/snippet.js?key=331b3ea0-7afc-44ad-8e2f-d2fc3e5a36c3';
+    script.src = 'https://static.zdassets.com/ekr/snippet.js?key=afc596a6-9089-437a-8022-ff54b88bd0cf';
     script.async = true;
     document.body.appendChild(script);
 
